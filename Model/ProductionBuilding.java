6a92c4825d1e4d1881d4288e246108d1db8d2a1c
Hoora <hooraabedin2004@gamil.com>
2023-04-18 21:53:09 +0430

    ProductionBuilding removed!

--- Model/ProductionBuilding.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-public class ProductionBuilding extends Building{
-    private int rate;
-    public ProductionBuilding(Building building, Cell location, User king, int rate) {
-        super(building, location, king);
-        this.rate = rate;
-    }
-}
