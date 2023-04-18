d35ebef6d6af8c86e72c39abd6e837be66318ddd
Melika Alizadeh <Melikaalizadeh7@gmail.com>
2023-04-18 21:41:48 +0330

    Cell class updated

--- Model/Cell.java
+++ Model/Cell.java
@@ -5,29 +5,29 @@ import java.util.ArrayList;
 public class Cell {
     private String Material;
     private Building building;
-    private ArrayList<Person> people=new ArrayList<Person>();
-    private ArrayList<NaturalBlock> naturalBlocks=new ArrayList<NaturalBlock>();
+    private ArrayList<Person> people = new ArrayList<Person>();
+    private ArrayList<NaturalBlock> naturalBlocks = new ArrayList<NaturalBlock>();
     private boolean hasTunnel;
 
 
-    public void addPerson(Person person){
-
-    }
-
-    public void setMaterial(String material) {
-        Material = material;
-    }
-
-    public void setBuilding(Building building) {
-        this.building = building;
+    public void addPerson(Person person) {
+        people.add(person);
     }
 
     public String getMaterial() {
         return Material;
     }
 
+    public void setMaterial(String material) {
+        Material = material;
+    }
+
     public Building getBuilding() {
         return building;
+    }
+
+    public void setBuilding(Building building) {
+        this.building = building;
     }
 
     public ArrayList<Person> getPeople() {
