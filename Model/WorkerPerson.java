87631acea71e49918e4b9be1946e7cc1f8d5e22b
Melika Alizadeh <Melikaalizadeh7@gmail.com>
2023-04-19 10:14:22 +0330

    WorkerPerson class updated

--- Model/WorkerPerson.java
+++ Model/WorkerPerson.java
@@ -2,20 +2,14 @@ package Model;
 
 public class WorkerPerson extends Person {
     private Building workerPlace;
-    private String name;
 
 
-    public WorkerPerson(User king, String type, Cell location, Building workerPlace, String name) {
+    public WorkerPerson(User king, String type, Cell location, Building workerPlace) {
         super(king, type, location);
         this.workerPlace = workerPlace;
-        this.name = name;
     }
 
     public Building getWorkerPlace() {
         return workerPlace;
-    }
-
-    public String getName() {
-        return name;
     }
 }
