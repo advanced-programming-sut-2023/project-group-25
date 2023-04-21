f65f6a9ffff12fd6d6acb0a0cc1026e217bca054
Hoora <hooraabedin2004@gamil.com>
2023-04-21 15:22:09 +0430

    updating

--- View/GameMenu.java
+++ View/GameMenu.java
@@ -3,6 +3,7 @@ package View;
 import java.util.Scanner;
 
 public class GameMenu {
+    //testing
     private GameController controller;
     public GameMenu(GameController gameController) {
         this.controller = gameController;
