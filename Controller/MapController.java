6efed3223e56152faa281241be07a392db952246
samin <saminakbari.e@gmail.com>
2023-04-24 14:43:13 +0330

    map materials and colors updated

--- Controller/MapController.java
+++ Controller/MapController.java
@@ -10,14 +10,16 @@ public class MapController {
     public static final String GREEN_BACKGROUND_BRIGHT = "\033[0;102m";// LIGHT GREEN
     public static final String YELLOW_BACKGROUND_BRIGHT = "\033[0;103m";// LIGHT YELLOW
     public static final String YELLOW_BACKGROUND = "\033[43m"; // YELLOW
+    public static final String BLACK_BACKGROUND_BRIGHT = "\033[0;100m";// BLACK
+    public static final String BLUE_BACKGROUND = "\033[44m";   // BLUE
+    public static final String GREEN_BACKGROUND = "\033[42m";  // GREEN
+    public static final String PURPLE_BACKGROUND = "\033[45m"; // PURPLE
+    
     
     //extra colors:
 //    public static final String BLACK_BACKGROUND = "\033[40m";   // BLACK
-//    public static final String GREEN_BACKGROUND = "\033[42m";  // GREEN
-//    public static final String PURPLE_BACKGROUND = "\033[45m"; // PURPLE
 //    public static final String CYAN_BACKGROUND = "\033[46m";   // CYAN
 //    public static final String WHITE_BACKGROUND = "\033[47m";  // WHITE
-//    public static final String BLACK_BACKGROUND_BRIGHT = "\033[0;100m";// BLACK
 //    public static final String PURPLE_BACKGROUND_BRIGHT = "\033[0;105m"; // PURPLE
 //    public static final String CYAN_BACKGROUND_BRIGHT = "\033[0;106m";  // CYAN
 //    public static final String WHITE_BACKGROUND_BRIGHT = "\033[0;107m";   // WHITE
@@ -29,17 +31,32 @@ public class MapController {
         for (int i = 0; i < mapLength; i++) {
             for (int j = 0; j < mapWidth; j++) {
                 switch (map.getCells()[i][j].getMaterial()) {
-                    case "land":
-                        mapView += YELLOW_BACKGROUND_BRIGHT + "######\n######\n######\n" + BACKGROUND_RESET;
+                    case "ironLand":
+                        mapView += RED_BACKGROUND + "#" + BACKGROUND_RESET;
                         break;
-                    case "water":
-                        mapView += BLUE_BACKGROUND_BRIGHT + "######\n######\n######\n" + BACKGROUND_RESET;
+                    case "rockLand":
+                        mapView += BLACK_BACKGROUND_BRIGHT + "#" + BACKGROUND_RESET;
+                        break;
+                    case "sea":
+                        mapView += BLUE_BACKGROUND + "#" + BACKGROUND_RESET;
+                        break;
+                    case "lowWater":
+                        mapView += BLUE_BACKGROUND_BRIGHT + "#" + BACKGROUND_RESET;
                         break;
                     case "grass":
-                        mapView += GREEN_BACKGROUND_BRIGHT + "######\n######\n######\n" + BACKGROUND_RESET;
+                        mapView += GREEN_BACKGROUND_BRIGHT + "#" + BACKGROUND_RESET;
+                        break;
+                    case "jolge":
+                        mapView += GREEN_BACKGROUND + "#" + BACKGROUND_RESET;
                         break;
                     case "castle":
-                        mapView += RED_BACKGROUND + "######\n######\n######\n" + BACKGROUND_RESET;
+                        mapView += PURPLE_BACKGROUND + "#" + BACKGROUND_RESET;
+                        break;
+                    case "beach":
+                        mapView += YELLOW_BACKGROUND_BRIGHT + "#" + BACKGROUND_RESET;
+                        break;
+                    default:
+                        mapView += YELLOW_BACKGROUND + "#" + BACKGROUND_RESET;
                         break;
                 }
             }
