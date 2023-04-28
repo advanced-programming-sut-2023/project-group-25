f0566146d7ec30ba1f7b8448d2691b62ec051453
Melika Alizadeh <Melikaalizadeh7@gmail.com>
2023-04-28 23:17:48 +0330

    Natural Block updated

--- Model/NaturalBlock.java
+++ Model/NaturalBlock.java
@@ -3,12 +3,17 @@ package Model;
 public class NaturalBlock {
     private String name;
     private String category;
+    private String direction;
     
     public NaturalBlock(String name, String category) {
         this.name = name;
         this.category = category;
     }
-    
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
     public String getName() {
         return name;
     }
