e5e858f868c08adb339f795e1219f4870ca9e001
hoora-abedin <119754746+hoora-abedin@users.noreply.github.com>
2023-06-02 13:05:22 +0330

    ghalat kardam

--- Phase2/AP-Project/src/main/java/Model/MainController.java
+++
@@ -1,7 +0,0 @@
-package Model;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
-public class MainController {
-}
--- Phase2/AP-Project/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.approject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens Model to javafx.fxml;
-    exports Model;
-}
