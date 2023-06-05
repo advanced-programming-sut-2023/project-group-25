03043a0233a4f9ba19a6c9014ef02bd0057e6e8c
Melika Alizadeh <Melikaalizadeh7@gmail.com>
2023-06-05 11:53:29 +0330

    added menu in game

--- Phase 2/AP-Project/src/main/java/Controller/MapController2.java
+++ Phase 2/AP-Project/src/main/java/Controller/MapController2.java
@@ -2,32 +2,73 @@ package Controller;
 
 import Model.*;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class MapController2 {
+
+    private final ImageView imageView = new ImageView(new Image(String.valueOf(getClass().getResource("/images/menu.png"))));
+    private final ImageView imageIcon1 = new ImageView(new Image(String.valueOf(getClass().getResource("/images/i1.png"))));
+    private final ImageView imageIcon2 = new ImageView(new Image(String.valueOf(getClass().getResource("/images/i2.png"))));
+    private final ImageView imageIcon3 = new ImageView(new Image(String.valueOf(getClass().getResource("/images/i3.png"))));
+    private final ImageView imageIcon4 = new ImageView(new Image(String.valueOf(getClass().getResource("/images/i4.png"))));
+    private final ImageView imageIcon5 = new ImageView(new Image(String.valueOf(getClass().getResource("/images/i5.png"))));
+    private final ImageView imageIcon6 = new ImageView(new Image(String.valueOf(getClass().getResource("/images/i6.png"))));
+    private final ImageView barracks = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/TrainingBuilding/barracks0.png"))));
+    private final ImageView mercenary = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/TrainingBuilding/mercenary0.png"))));
+    private final ImageView armoury = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/StorageBuilding/armoury0.png"))));
+    private final ImageView stairs = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/OtherBuilding/stairs.png"))));
+    private final ImageView shortWall = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/OtherBuilding/short wall.png"))));
+    private final ImageView highWall = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/OtherBuilding/high wall.png"))));
+    private final ImageView stockpile = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/StorageBuilding/stockpile.png"))));
+    private final ImageView woodCutter = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/ProductionBuilding/woodcutter.png"))));
+    private final ImageView quarry = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/ProductionBuilding/quarry.gif"))));
+    private final ImageView oxTether = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/ProductionBuilding/ox tether.gif"))));
+    private final ImageView ironMine = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/ProductionBuilding/iron mine.png"))));
+    private final ImageView market = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/ShopBuilding/market0.png"))));
+    private final ImageView appleOrchard = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/ProductionBuilding/appleOrchard.png"))));
+    private final ImageView dairyFarmer = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/ProductionBuilding/dairyFarmer.png"))));
+    private final ImageView hopsFarmer = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/ProductionBuilding/hopsFarmer.png"))));
+    private final ImageView wheatFarmer = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/ProductionBuilding/wheatFarmer.png"))));
+    private final ImageView hovel = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/OtherBuilding/hovel.png"))));
+    private final ImageView church = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/OtherBuilding/church.png"))));
+    private final ImageView catheral = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/OtherBuilding/catheral.png"))));
+    private final ImageView armourer = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/ProductionBuilding/armourer.png"))));
+    private final ImageView blacksmith = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/ProductionBuilding/blacksmith.png"))));
+    private final ImageView fletcher = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/ProductionBuilding/fletcher.png"))));
+    private final ImageView poleturner = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/ProductionBuilding/poleturner.png"))));
+    private final ImageView granary = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/StorageBuilding/granary0.png"))));
+    private final ImageView bakery = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/ProductionBuilding/bakery.png"))));
+    private final ImageView brewer = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/ProductionBuilding/brewer.png"))));
+    private final ImageView mill = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/ProductionBuilding/mill.gif"))));
+    private final ImageView inn = new ImageView(new Image(String.valueOf(getClass().getResource("/images/Buildings/OtherBuilding/inn.png"))));
+
     public void loadMapToShow(Stage stage, Pane pane, Map map, int x, int y) {
-        //capacity: 31 x 16
-        
+        //capacity: 31 x 15
+
         if (!isLocationAppropriateToShow(x, y, map)) {
+            System.out.println("1");
             return;
         }
-        
+
         int xCounter = 0, yCounter = 0;
-        
+
         for (int i = x - 15; i < x + 16; i++) {
-            for (int j = y - 7; j < y + 9 ; j++) {
+            for (int j = y - 7; j < y + 8; j++) {
                 Cell cell = map.getCells()[i][j];
-                
+
                 showBackground(pane, cell.getMaterial(), xCounter, yCounter);
-                
+
                 for (NaturalBlock naturalBlock : cell.getNaturalBlocks()) {
                     showNaturalBlock(pane, i, j, naturalBlock);
                 }
-                
+
                 showBuilding(pane, map, i, j, cell.getBuilding());
-    
+
                 for (Person person : cell.getPeople()) {
                     showPerson(pane, map, i, j, person);
                 }
@@ -36,16 +77,295 @@ public class MapController2 {
             xCounter++;
             yCounter = 0;
         }
-        
-        //TODO: bar below the page
-        
+        imageView.setLayoutX(0);
+        imageView.setLayoutY(620);
+
+        imageIcon1.setLayoutX(500);
+        imageIcon1.setLayoutY(760);
+        imageIcon1.setFitHeight(50);
+        imageIcon1.setFitWidth(50);
+        setMenuIcon1();
+        setImagesIcons2(false);
+        setImagesIcons3(false);
+        setImagesIcons4(false);
+        setImagesIcons5(false);
+        setImagesIcons6(false);
+        imageIcon1.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            setImagesIcons1(true);
+            setImagesIcons2(false);
+            setImagesIcons3(false);
+            setImagesIcons4(false);
+            setImagesIcons5(false);
+            setImagesIcons6(false);
+            event.consume();
+        });
+
+        imageIcon2.setLayoutX(550);
+        imageIcon2.setLayoutY(770);
+        imageIcon2.setFitHeight(40);
+        imageIcon2.setFitWidth(40);
+        setMenuIcon2();
+        imageIcon2.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            setImagesIcons1(false);
+            setImagesIcons2(true);
+            setImagesIcons3(false);
+            setImagesIcons4(false);
+            setImagesIcons5(false);
+            setImagesIcons6(false);
+            event.consume();
+        });
+
+        imageIcon3.setLayoutX(600);
+        imageIcon3.setLayoutY(767.5);
+        imageIcon3.setFitHeight(45);
+        imageIcon3.setFitWidth(40);
+        setMenuIcon3();
+        imageIcon3.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            setImagesIcons1(false);
+            setImagesIcons2(false);
+            setImagesIcons3(true);
+            setImagesIcons4(false);
+            setImagesIcons5(false);
+            setImagesIcons6(false);
+            event.consume();
+        });
+
+        imageIcon4.setLayoutX(650);
+        imageIcon4.setLayoutY(767.5);
+        imageIcon4.setFitHeight(45);
+        imageIcon4.setFitWidth(40);
+        setMenuIcon4();
+        imageIcon4.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            setImagesIcons1(false);
+            setImagesIcons2(false);
+            setImagesIcons3(false);
+            setImagesIcons4(true);
+            setImagesIcons5(false);
+            setImagesIcons6(false);
+            event.consume();
+        });
+
+        imageIcon5.setLayoutX(700);
+        imageIcon5.setLayoutY(770);
+        imageIcon5.setFitHeight(40);
+        imageIcon5.setFitWidth(40);
+        setMenuIcon5();
+        imageIcon5.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            setImagesIcons1(false);
+            setImagesIcons2(false);
+            setImagesIcons3(false);
+            setImagesIcons4(false);
+            setImagesIcons5(true);
+            setImagesIcons6(false);
+            event.consume();
+        });
+
+        imageIcon6.setLayoutX(750);
+        imageIcon6.setLayoutY(770);
+        imageIcon6.setFitHeight(40);
+        imageIcon6.setFitWidth(40);
+        setMenuIcon6();
+        imageIcon6.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            setImagesIcons1(false);
+            setImagesIcons2(false);
+            setImagesIcons3(false);
+            setImagesIcons4(false);
+            setImagesIcons5(false);
+            setImagesIcons6(true);
+            event.consume();
+        });
+
+
+        pane.getChildren().addAll(imageView, imageIcon1, imageIcon2, imageIcon3, imageIcon4, imageIcon5, imageIcon6
+                , barracks, mercenary, armoury, stairs, shortWall, highWall
+                , stockpile, woodCutter, quarry, ironMine, oxTether, market
+                , appleOrchard, wheatFarmer, hopsFarmer, dairyFarmer
+                , hovel, church, catheral
+                , poleturner, armourer, blacksmith, fletcher
+                , granary, bakery, brewer, mill, inn);
         stage.show();
     }
-    
+
+    private void setMenuIcon6() {
+        granary.setLayoutX(500);
+        granary.setLayoutY(690);
+        granary.setFitHeight(70);
+        granary.setFitWidth(70);
+        bakery.setLayoutX(580);
+        bakery.setLayoutY(690);
+        bakery.setFitHeight(70);
+        bakery.setFitWidth(70);
+        brewer.setLayoutX(650);
+        brewer.setLayoutY(690);
+        brewer.setFitHeight(70);
+        brewer.setFitWidth(70);
+        mill.setLayoutX(750);
+        mill.setLayoutY(690);
+        mill.setFitHeight(70);
+        mill.setFitWidth(50);
+        inn.setLayoutX(840);
+        inn.setLayoutY(690);
+        inn.setFitHeight(70);
+        inn.setFitWidth(70);
+    }
+
+    private void setImagesIcons6(boolean check) {
+        granary.setVisible(check);
+        bakery.setVisible(check);
+        brewer.setVisible(check);
+        mill.setVisible(check);
+        inn.setVisible(check);
+    }
+
+    private void setMenuIcon5() {
+        fletcher.setLayoutX(500);
+        fletcher.setLayoutY(690);
+        fletcher.setFitHeight(70);
+        fletcher.setFitWidth(70);
+        poleturner.setLayoutX(590);
+        poleturner.setLayoutY(690);
+        poleturner.setFitWidth(70);
+        poleturner.setFitHeight(70);
+        blacksmith.setLayoutX(670);
+        blacksmith.setLayoutY(690);
+        blacksmith.setFitHeight(70);
+        blacksmith.setFitWidth(70);
+        armourer.setLayoutX(750);
+        armourer.setLayoutY(690);
+        armourer.setFitWidth(70);
+        armourer.setFitHeight(70);
+    }
+
+    private void setImagesIcons5(boolean check) {
+        poleturner.setVisible(check);
+        fletcher.setVisible(check);
+        blacksmith.setVisible(check);
+        armourer.setVisible(check);
+    }
+
+    private void setMenuIcon4() {
+        hovel.setLayoutX(500);
+        hovel.setLayoutY(690);
+        hovel.setFitHeight(70);
+        hovel.setFitWidth(70);
+        church.setLayoutX(580);
+        church.setLayoutY(690);
+        church.setFitHeight(70);
+        church.setFitWidth(70);
+        catheral.setLayoutX(660);
+        catheral.setLayoutY(690);
+        catheral.setFitHeight(80);
+        catheral.setFitWidth(80);
+    }
+
+    private void setImagesIcons4(boolean check) {
+        hovel.setVisible(check);
+        church.setVisible(check);
+        catheral.setVisible(check);
+    }
+
+    private void setMenuIcon3() {
+        appleOrchard.setLayoutX(500);
+        appleOrchard.setLayoutY(690);
+        appleOrchard.setFitHeight(70);
+        appleOrchard.setFitWidth(70);
+        dairyFarmer.setLayoutX(580);
+        dairyFarmer.setLayoutY(690);
+        dairyFarmer.setFitHeight(70);
+        dairyFarmer.setFitWidth(70);
+        hopsFarmer.setLayoutX(670);
+        hopsFarmer.setLayoutY(690);
+        hopsFarmer.setFitHeight(70);
+        hopsFarmer.setFitWidth(70);
+        wheatFarmer.setLayoutX(750);
+        wheatFarmer.setLayoutY(690);
+        wheatFarmer.setFitHeight(70);
+        wheatFarmer.setFitWidth(70);
+    }
+
+    private void setImagesIcons3(boolean check) {
+        appleOrchard.setVisible(check);
+        dairyFarmer.setVisible(check);
+        hopsFarmer.setVisible(check);
+        wheatFarmer.setVisible(check);
+    }
+
+    private void setMenuIcon2() {
+        stockpile.setLayoutX(500);
+        stockpile.setLayoutY(690);
+        stockpile.setFitHeight(70);
+        stockpile.setFitWidth(70);
+        woodCutter.setLayoutX(580);
+        woodCutter.setLayoutY(690);
+        woodCutter.setFitHeight(70);
+        woodCutter.setFitHeight(70);
+        quarry.setLayoutX(690);
+        quarry.setLayoutY(690);
+        quarry.setFitHeight(70);
+        quarry.setFitWidth(70);
+        oxTether.setLayoutX(750);
+        oxTether.setLayoutY(680);
+        oxTether.setFitHeight(50);
+        oxTether.setFitWidth(50);
+        ironMine.setLayoutX(800);
+        ironMine.setLayoutY(690);
+        ironMine.setFitHeight(70);
+        ironMine.setFitWidth(70);
+        market.setLayoutX(880);
+        market.setLayoutY(690);
+        market.setFitHeight(70);
+        market.setFitWidth(70);
+    }
+
+    private void setMenuIcon1() {
+        barracks.setLayoutX(500);
+        barracks.setLayoutY(690);
+        barracks.setFitHeight(70);
+        barracks.setFitWidth(70);
+        mercenary.setLayoutX(580);
+        mercenary.setLayoutY(690);
+        mercenary.setFitHeight(70);
+        mercenary.setFitWidth(70);
+        armoury.setLayoutX(640);
+        armoury.setLayoutY(680);
+        armoury.setFitHeight(90);
+        armoury.setFitWidth(100);
+        stairs.setLayoutX(750);
+        stairs.setLayoutY(690);
+        stairs.setFitHeight(70);
+        stairs.setFitWidth(20);
+        shortWall.setLayoutX(800);
+        shortWall.setLayoutY(690);
+        shortWall.setFitHeight(70);
+        shortWall.setFitWidth(20);
+        highWall.setLayoutX(850);
+        highWall.setLayoutY(690);
+        highWall.setFitHeight(70);
+        highWall.setFitWidth(20);
+    }
+
+    private void setImagesIcons2(boolean check) {
+        stockpile.setVisible(check);
+        woodCutter.setVisible(check);
+        quarry.setVisible(check);
+        oxTether.setVisible(check);
+        ironMine.setVisible(check);
+        market.setVisible(check);
+    }
+
+    private void setImagesIcons1(boolean check) {
+        barracks.setVisible(check);
+        mercenary.setVisible(check);
+        armoury.setVisible(check);
+        stairs.setVisible(check);
+        highWall.setVisible(check);
+        shortWall.setVisible(check);
+    }
+
     private boolean isLocationAppropriateToShow(int x, int y, Map map) {
         return x - 15 >= 0 && y - 7 >= 0 && x + 16 <= map.getWidth() && y + 8 <= map.getLength();
     }
-    
+
     private void showNaturalBlock(Pane pane, int i, int j, NaturalBlock naturalBlock) {
 //        ImageView imageView = new ImageView();
 //        imageView.setFitHeight(30);
@@ -56,25 +376,25 @@ public class MapController2 {
 //        imageView.setImage(new Image(Objects.requireNonNull(getClass().getResource(address)).toExternalForm()));
 //        pane.getChildren().add(imageView);
     }
-    
+
     private void showPerson(Pane pane, Map map, int i, int j, Person person) {
         //TODO
     }
-    
+
     private void showBackground(Pane pane, String material, int x, int y) {
         Label imageView = new Label();
         imageView.setPrefWidth(50);
         imageView.setPrefHeight(50);
-        imageView.setLayoutX(50*x);
-        imageView.setLayoutY(50*y);
-        imageView.setStyle("-fx-border-color: #ffffff; -fx-border-width: 0.2px; -fx-border-style: dash");
+        imageView.setLayoutX(50 * x);
+        imageView.setLayoutY(50 * y);
+        imageView.setStyle("-fx-border-color: #ffffff; -fx-border-width: 0.5px; -fx-border-style: dash");
         String address = "/images/" + material + ".jpg";
         Background background = new Background(MainController.setFirstPageBackground(address));
         imageView.setBackground(background);
         pane.getChildren().add(imageView);
         pane.setStyle("-fx-spacing: 0");
     }
-    
+
     private void showBuilding(Pane pane, Map map, int i, int j, Building building) {
         //TODO
     }
